33659705ddb4b7062d325da2c10981b21a571c81
Aaron Newman <aaronnewman@Aarons-MBP.T-mobile.com>
2017-02-18 19:47:04 -0600

    Cleaned up project a bit and renamed to ngx

--- index.ts
+++ index.ts
@@ -1,2 +1,21 @@
-export * from "./src/loggly-service";
-export class LogglyModule {}
+import {NgModule, ModuleWithProviders} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LogglyService} from './src/loggly.service';
+
+export * from './src/loggly.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [],
+  exports: []
+})
+export class NgxLogglyModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: NgxLogglyModule,
+      providers: [LogglyService]
+    };
+  }
+}
